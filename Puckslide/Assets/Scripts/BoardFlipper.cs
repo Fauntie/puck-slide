1418b16b4e6764774de9676e9aca185f36f26e54
Fauntie <sebenkelman@gmail.com>
2025-08-10 18:19:39 +0200

    Fix board flip around center

--- Puckslide/Assets/Scripts/BoardFlipper.cs
+++ Puckslide/Assets/Scripts/BoardFlipper.cs
@@ -3,16 +3,49 @@ using UnityEngine;
 public static class BoardFlipper
 {
     private static bool s_IsFlipped = false;
+    private static Transform s_BoardTransform;
+    private static int s_GridSize;
+    private static float s_TileSize;
+
+    public static void SetBoard(Transform board, int gridSize, float tileSize)
+    {
+        s_BoardTransform = board;
+        s_GridSize = gridSize;
+        s_TileSize = tileSize;
+    }
+
+    private static Vector3 GetBoardCenter()
+    {
+        return s_BoardTransform.position + new Vector3(s_GridSize * s_TileSize / 2f, s_GridSize * s_TileSize / 2f, 0f);
+    }
 
     public static void Flip()
     {
-        if (Camera.main == null)
+        if (s_BoardTransform == null)
         {
             return;
         }
 
         s_IsFlipped = !s_IsFlipped;
-        float angle = s_IsFlipped ? 180f : 0f;
-        Camera.main.transform.rotation = Quaternion.Euler(0f, 0f, angle);
+        Vector3 boardCenter = GetBoardCenter();
+        s_BoardTransform.RotateAround(boardCenter, Vector3.forward, 180f);
+
+        foreach (PuckController puck in Object.FindObjectsOfType<PuckController>())
+        {
+            if (!puck.transform.IsChildOf(s_BoardTransform))
+            {
+                puck.transform.RotateAround(boardCenter, Vector3.forward, 180f);
+            }
+            puck.transform.rotation = Quaternion.identity;
+        }
+
+        foreach (Piece piece in Object.FindObjectsOfType<Piece>())
+        {
+            if (!piece.transform.IsChildOf(s_BoardTransform))
+            {
+                piece.transform.RotateAround(boardCenter, Vector3.forward, 180f);
+            }
+            piece.transform.rotation = Quaternion.identity;
+        }
     }
 }
