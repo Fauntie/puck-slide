57a3ffcc31b5be2df107b0af6c34394a4cf949d9
Fauntie <sebenkelman@gmail.com>
2025-08-16 21:23:42 +0200

    Revert "Replace unsupported C# constructs"

--- Puckslide/Assets/Scripts/PuckController.cs
+++ Puckslide/Assets/Scripts/PuckController.cs
@@ -59,10 +59,7 @@ public class PuckController : MonoBehaviour
     {
         m_Camera = Camera.main;
         m_Rigidbody.freezeRotation = true;
-        if (m_TrajectoryRenderer == null)
-        {
-            m_TrajectoryRenderer = GetComponent<LineRenderer>();
-        }
+        m_TrajectoryRenderer ??= GetComponent<LineRenderer>();
         if (m_TrajectoryRenderer == null)
         {
             Debug.LogWarning("PuckController requires a LineRenderer component.", this);
